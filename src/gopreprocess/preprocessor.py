91fd17a06b46a3dab8c9d2107a1f9133ce6443dd
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-06-12 11:18:19 -0700

    update poetry.lock

--- src/gopreprocess/preprocessor.py
+++ src/gopreprocess/preprocessor.py
@@ -6,7 +6,6 @@ from src.utils.settings import get_alliance_ortho_url
 def preprocess():
     # Usage example
     path = pystow.ensure_gunzip('ORTHO', url=get_alliance_ortho_url(), autoclean=True, force=True)
-    print(path)
     ortho_processor = OrthoProcessor(path)
     data = ortho_processor.get_data()
     print(data.get("metadata"))
