25f8658d029d64763d3499ba2430e2364ddfe429
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-06-12 17:12:35 -0700

    add some continues

--- src/gopreprocess/preprocessor.py
+++ src/gopreprocess/preprocessor.py
@@ -13,8 +13,13 @@ def preprocess():
     ortho_path, rgd_gaf_path, mgi_gpi_path = download_files()
     mouse_genes = GpiProcessor(mgi_gpi_path).genes
     rat_genes = OrthoProcessor(mouse_genes, ortho_path, mouse_taxon, rat_taxon).genes
+    for k, v in rat_genes.items():  # exclude any rat genes whose orthologs are not in the mouse GPI file.
+        if v not in mouse_genes:
+            del rat_genes[k]
     rgd_annotations = GafProcessor(rat_genes, rgd_gaf_path, namespaces=namespaces).convertable_annotations
     rat_gene_set = set(rat_genes.keys())
+    print(list(rat_gene_set)[0])
+    print(list(rat_gene_set)[1])
     for annotation in rgd_annotations:
         print(annotation.subject.id)
         if annotation.subject.id in rat_gene_set:
