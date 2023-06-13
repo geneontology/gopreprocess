785026b53b8608f6a5f5fb4a90b768ddbd2e5860
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-06-13 15:20:01 -0700

    simplify config

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -8,54 +8,6 @@ CONFIG = path.join(path.dirname(path.abspath(__file__)), "../config/download_con
 logger = logging.getLogger(__name__)
 
 
-def get_alliance_ortho_url():
-    """
-    Retrieves the Alliance orthology URL from the configuration file.
-
-    :return: The Alliance orthology URL.
-    :rtype: str
-    """
-    with open(CONFIG, "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    return config["Ortho"]["alliance_ortho_url"]
-
-
-def get_mgi_gaf_url():
-    """
-    Retrieves the MGI GAF URL from the configuration file.
-
-    :return: The MGI GAF URL.
-    :rtype: str
-    """
-    with open(CONFIG, "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    return config["MGI"]["mgi_goa_url"]
-
-
-def get_mgi_gpi_url():
-    """
-    Retrieves the MGI GPI URL from the configuration file.
-
-    :return: The MGI GPI URL.
-    :rtype: str
-    """
-    with open(CONFIG, "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    return config["MGI"]["mgi_gpi_url"]
-
-
-def get_rgd_gpad_url():
-    """
-    Retrieves the RGD GPAD URL from the configuration file.
-
-    :return: The RGD GPAD URL.
-    :rtype: str
-    """
-    with open(CONFIG, "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    return config["RGD"]["rgd_gaf_url"]
-
-
 def get_url(key: str) -> str:
     """
     Retrieves the URL corresponding to the given key from the configuration file.
