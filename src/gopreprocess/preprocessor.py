7ad0dcba3880fac54d2dc899768730da464a7465
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-06-14 14:02:29 -0700

    add MGI gene name and info to inferred ortho annotations

--- src/gopreprocess/preprocessor.py
+++ src/gopreprocess/preprocessor.py
@@ -7,12 +7,12 @@ from ontobio.io.assocwriter import GpadWriter
 import time
 import pandas as pd
 import pystow
-from typing import Mapping
+from typing import List
 
 namespaces = ["RGD", "UniProtKB"]
-mouse_taxon = "taxon:10090"
-rat_taxon = "taxon:10116"
-human_taxon = "taxon:9606"
+mouse_taxon = "NCBITaxon:10090"
+rat_taxon = "NCBITaxon:10116"
+human_taxon = "NCBITaxon:9606"
 iso_code = "0000266"
 protein_coding_gene = "SO:0001217"
 ortho_reference = "0000096"
@@ -30,8 +30,8 @@ def preprocess() -> None:
 
     # assemble data structures needed to convert annotations via ortholog relationships.
     ortho_path, rgd_gaf_path, mgi_gpi_path = download_files()
-    mouse_genes = GpiProcessor(mgi_gpi_path).genes
-    rat_genes = OrthoProcessor(mouse_genes, ortho_path, mouse_taxon, rat_taxon).genes
+    source_genes = GpiProcessor(mgi_gpi_path).genes
+    rat_genes = OrthoProcessor(source_genes, ortho_path, mouse_taxon, rat_taxon).genes
     rgd_annotations = GafProcessor(rat_genes, rgd_gaf_path, namespaces=namespaces).convertible_annotations
 
     # just for performance of the check below for rat genes in the RGD GAF file that have
@@ -62,7 +62,7 @@ def preprocess() -> None:
     print("time to execute", end - start)
 
 
-def generate_annotation(annotation: GoAssociation, gene_map: dict) -> GoAssociation:
+def generate_annotation(annotation: GoAssociation, gene_map: dict, source_genes: List[dict]) -> GoAssociation:
     """
     Generates a new annotation based on ortholog assignments.
 
@@ -73,6 +73,7 @@ def generate_annotation(annotation: GoAssociation, gene_map: dict) -> GoAssociat
 
     :raises KeyError: If the gene ID is not found in the gene map.
     """
+
     # rewrite with MGI gene ID
     annotation.evidence.has_supporting_reference = [Curie(namespace='GO_REF', identity=ortho_reference)]
     annotation.evidence.type = Curie(namespace='ECO', identity=iso_code) # all annotations via ortho should have this ECO code
@@ -83,10 +84,18 @@ def generate_annotation(annotation: GoAssociation, gene_map: dict) -> GoAssociat
     annotation.subject.fullname = []
     annotation.subject.label = ""
     annotation.subject.synonyms = []
-    annotation.object.taxon.namespace = 'taxon'
     annotation.object.taxon = Curie.from_str(mouse_taxon)
     if annotation.provided_by == "RGD":
         annotation.provided_by = "MGI"
+
+    # get mouse gene details
+    for gene in source_genes:
+        if gene["id"] == str(annotation.subject.id):
+            annotation.subject.fullname = [gene["fullname"]]
+            annotation.subject.label = gene["label"]
+            annotation.subject.synonyms = gene["synonyms"]
+            break
+
     return annotation
 
 
