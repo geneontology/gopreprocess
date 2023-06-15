23602a9966ad23a6d762a22bcf2140899c6c5517
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-06-15 11:05:25 -0700

    more genericizing

--- src/gopreprocess/cli.py
+++ src/gopreprocess/cli.py
@@ -7,7 +7,7 @@ from src.gopreprocess.annotation_converter import AnnotationConverter
                                                                  "used to retrieve source annotations for conversion.")
 @click.option("--target_taxon", default="NCBITaxon:10090", help="Target taxon in curie format using NCBITaxon prefix.")
 @click.option("--source_taxon", default="NCBITaxon:10116", help="Source taxon in curie format using NCBITaxon prefix.")
-@click.option("--ortho_reference", default="0000096", help="Ortho reference in curie format.")
+@click.option("--ortho_reference", default="GO_REF:0000096", help="Ortho reference in curie format.")
 def convert_annotations(namespaces, target_taxon, source_taxon, ortho_reference):
     converter = AnnotationConverter(namespaces, target_taxon, source_taxon, ortho_reference)
     converter.convert_annotations()
