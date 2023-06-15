8adfd45b39b67089e460aa592b6cc92c09c5ae05
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-06-15 11:44:11 -0700

    adding tests

--- tests/test_gpi_processor.py
+++ tests/test_gpi_processor.py
@@ -6,9 +6,10 @@ from pathlib import Path
 
 class TestGPIProcessor(unittest.TestCase):
 
-    def test_get_gpi(self):
+    def test_parse_gpi(self):
         data_path = os.path.expanduser("~/.data/MGI/")
         file_path = Path(os.path.join(data_path, "mgi.gpi"))
         self.assertTrue(file_path.exists())
         gpi_genes = GpiProcessor(file_path)
         self.assertTrue(type(gpi_genes.target_genes) == dict)
+        self.assertTrue(len(gpi_genes.target_genes) > 0)
