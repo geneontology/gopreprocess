e5a26900a12400bed620b8f36d68ae7ddda58e6f
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-10-17 14:58:34 -0700

    update gaf processing for Protein2GO

--- src/gopreprocess/file_processors/protein_to_go_processor.py
+++ src/gopreprocess/file_processors/protein_to_go_processor.py
@@ -2,6 +2,9 @@
 
 from src.utils.decorators import timer
 from src.utils.download import download_file
+from src.gopreprocess.file_processors.gafprocessor import GafProcessor
+from src.gopreprocess.file_processors.xref_processor import XrefProcessor
+from src.utils.settings import taxon_to_provider
 
 
 @timer
@@ -11,5 +14,18 @@ def add_protein_to_go_files():
 
     :return: None
     """
-    download_file(target_directory_name="GAF_OUTPUT", config_key="GOA_MOUSE", gunzip=True)
-    download_file(target_directory_name="GAF_OUTPUT", config_key="GOA_MOUSE_ISOFORM", gunzip=True)
+    xrefs = XrefProcessor()
+    uniprot_to_hgnc_map = xrefs.uniprot_to_hgnc_map
+
+    source_gaf_path = download_file(target_directory_name="GAF_OUTPUT", config_key="GOA_MOUSE", gunzip=True)
+    source_isoform_gaf_path = download_file(target_directory_name="GAF_OUTPUT", config_key="GOA_MOUSE_ISOFORM", gunzip=True)
+
+
+    # Process the GAF files
+    source_annotations = GafProcessor(
+        source_gaf_path,
+        taxon_to_provider=taxon_to_provider,
+        target_taxon=target_taxon,
+        namespaces=namespaces,
+        uniprot_to_hgnc_map=uniprot_to_hgnc_map,
+    ).convertible_annotations
