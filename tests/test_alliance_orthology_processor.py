78162d4f0463f9e19a5b51bd8dd764fb28b55dfe
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-03-14 17:24:55 -0700

    fixing up tests to use mock data instead of resources

--- tests/test_alliance_orthology_processor.py
+++ tests/test_alliance_orthology_processor.py
@@ -4,22 +4,3 @@ import os
 import unittest
 from pathlib import Path
 
-
-class TestOrthoProcessor(unittest.TestCase):
-
-    """Test the Alliance Ortholog processor."""
-
-    def test_parse_gpi(self):
-        """Test the files exist."""
-        data_path = os.path.expanduser("tests/resources")
-        file_path = Path(os.path.join(data_path, "test_ortho.json"))
-        print(file_path)
-        self.assertTrue(file_path.exists())
-
-
-if __name__ == "__main__":
-    """
-    Run the unit tests
-
-    """
-    unittest.main()
--- tests/test_ontology_processor.py
+++ tests/test_ontology_processor.py
@@ -6,22 +6,14 @@ from unittest import skip
 from gopreprocess.file_processors.ontology_processor import get_GO_aspector
 
 
-class TestOntologyProcessor(unittest.TestCase):
-
-    """Test the Ontology processor."""
-
-    @skip("go.json location is unknown to code until pipeline runs")
-    def test_ontology_processor(self):
-        """Test that the aspector is working to retrieve ancestors."""
-        go_aspector = get_GO_aspector("GO")
-        self.assertTrue(go_aspector.is_biological_process("GO:0051179"))
-        self.assertFalse(go_aspector.is_biological_process("GO_0051179"))
-        self.assertFalse(go_aspector.is_biological_process("0051179"))
-
-
-if __name__ == "__main__":
+@skip("go.json location is unknown to code until pipeline runs")
+def test_ontology_processor():
     """
-    Run the unit tests
+    Test that the aspector is working to retrieve ancestors.
 
     """
-    unittest.main()
+    go_aspector = get_GO_aspector("GO")
+    assert go_aspector.is_biological_process("GO:0051179") is True
+    assert go_aspector.is_biological_process("GO_0051179") is False
+    assert go_aspector.is_biological_process("0051179") is False
+
