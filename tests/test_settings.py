e3ff9cbba3b28365f35fedd67f4f61081ac6fbe1
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-05-30 11:10:43 -0700

    remove silver from URL downloads and tests, remove unused pypi GH action

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,6 +5,6 @@ from src.utils.settings import get_url
 
 def test_get_url():
     """Test the get_url function."""
-    expected_url = "http://skyhook.berkeleybop.org/silver-issue-325-gopreprocess/products/upstream_and_raw_data/mgi-src.gaf.gz"
-    actual_url = get_url("MGI_GAF")
+    expected_url = "http://snapshot.geneontology.org/products/upstream_and_raw_data/mgi-src.gpi.gz"
+    actual_url = get_url("MGI_GPI")
     assert actual_url == expected_url
